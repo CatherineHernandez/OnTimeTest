a68f4a4dd2eeba0274039e9ad6e701adb2dfcb71
CatyHernandez <cathy_9821@hotmail.com>
2018-08-01 09:48:03 -0700

    Log in y body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const appRoutes: Routes = [];
+import { AppComponent } from './app.component';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { BodyComponent } from './Components/RestofApp/body/body.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/LogIn', pathMatch: 'full' },
+    { path: 'LogIn', component: LogInComponent },
+    { path: 'Body', component: BodyComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
@@ -9,5 +17,4 @@ const appRoutes: Routes = [];
 })
 
 export class AppRoutingModule {
-    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { HeaderComponent } from './Components/RestofApp/header/header.component';
+import { BodyComponent } from './Components/RestofApp/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
